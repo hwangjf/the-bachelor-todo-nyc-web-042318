b72365fcd8b845ed23ea295ed936cc4c1031126a
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 13:28:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
   data.each do |season, contestant|
     contestant.each do |contestant|
       tot_age += contestant["age"].to_i
-      puts tot_age
+      return tot_age
     end
   end
 end
