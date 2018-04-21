e7cb823f80e8161b4242632e7c41f51c710154de
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 12:38:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,8 +23,9 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
+  counter = 0
   data.each do |season, contestant|
-    
+    if contestant
   end
 end
 
