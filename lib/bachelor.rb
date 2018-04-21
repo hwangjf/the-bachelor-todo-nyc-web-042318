f4bb9072ba2d84e8e59b5e25119aeb78966c6920
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 12:41:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -27,6 +27,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season, contestant|
     if contestant["hometown"] == hometown
       counter += 1
+    end
   end
   counter
 end
