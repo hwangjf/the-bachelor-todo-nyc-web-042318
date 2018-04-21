e8ab4c8d26ac8e7b1057828ad5d52e565c9229ba
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-21 12:36:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,31 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  a = []
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      a.push(contestant["name"])
+      for i in 0..a[0].length
+        if a[0][i] == " "
+          return a[0][0, i]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    
+  end
 end
 
 def get_occupation(data, hometown)
